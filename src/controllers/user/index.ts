dfb7e3a7a69a1fc2d73c1ffdfee5427d791a5254
Almeida <victor.freitas08@hotmail.com>
2021-08-18 21:59:21 -0300

    adding mongo client

--- src/controllers/user/index.ts
+++ src/controllers/user/index.ts
@@ -1,15 +1,34 @@
 import { auth, verify } from "../../security/indexJWT";
 import { Request, Response } from "express";
+import { FindUser, InsertUser } from "../../infra/mongo";
+import UserInterface from "../../model/userModel";
 
-const create = (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  return res.status(200).json({ created: true });
+const create = async (req: Request, res: Response) => {
+  try {
+    const { userName, email, password } = req.body;
+    const user: UserInterface = {
+      userName,
+      email: email,
+      password,
+    };
+    await InsertUser(user);
+    return res.status(200).json({ created: true });
+  } catch (error) {
+    return res.status(500).json({ created: false });
+  }
 };
-const login = (req: Request, res: Response) => {
+const login = async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const token = auth(email);
-  return res.status(200).json({ auth: true, token: token });
+  const user: UserInterface = {
+    userName: "",
+    email,
+    password,
+  };
+  const userFound = await FindUser(user);
+  return res
+    .status(200)
+    .json({ auth: true, token: token, userData: userFound });
 };
 const logout = (req: Request, res: Response) => {
   return res.status(200).json({ auth: false });
---
+++ src/infra/mongo.ts
@@ -0,0 +1,54 @@
+import { MongoClient } from "mongodb";
+import dotEnv from "dotenv";
+import UserInterface from "../model/userModel";
+dotEnv.config();
+
+const URL = process.env.MONGOURL;
+const DBNAME = process.env.DBNAME;
+const DBCOLECTION = process.env.DBCOLECTION;
+
+if (!URL) {
+  throw new Error("Error in mongoURL ");
+}
+if (!DBNAME) {
+  throw new Error("Error in mongodb name ");
+}
+if (!DBCOLECTION) {
+  throw new Error("Error in mongodb collection ");
+}
+var client = new MongoClient(URL);
+const ConnectDB = async () => {
+  try {
+    await client.connect();
+    const db = client.db(DBNAME);
+    const dbCollection = db.collection(DBCOLECTION);
+    return dbCollection;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export const InsertUser = async (user: UserInterface) => {
+  try {
+    const connected = await ConnectDB();
+    await connected.insertOne(user);
+    await client.close();
+  } catch (error) {
+    await client.close();
+    throw new Error(error);
+  }
+};
+export const FindUser = async (user: UserInterface) => {
+  try {
+    const connected = await ConnectDB();
+    const filterOne = {
+      email: user.email,
+    };
+    const userFound = await connected.findOne(filterOne);
+    await client.close();
+    return userFound;
+  } catch (error) {
+    await client.close();
+    throw new Error(error);
+  }
+};
