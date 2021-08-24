ba076f2de492ecec5bb40f9fe49859802b2a13fa
Almeida <victor.freitas08@hotmail.com>
2021-08-24 20:57:03 -0300

    refactor in signin

--- src/presentation/controllers/signIn.ts
+++ src/presentation/controllers/signIn.ts
@@ -1,4 +1,5 @@
 import AuthUseCase from "../../domain/useCase/authUseCase";
+
 import HttpResponse from "../helpers/http-response";
 
 class SignIn {
@@ -11,6 +12,7 @@ class SignIn {
       const { email, password } = httpRequest;
       if (!email || !password) throw new Error("invalid params");
       const authUser = await this.authUseCase.auth(email, password);
+      if (!authUser.auth) return HttpResponse.unauthorizeError();
       return HttpResponse.ok(authUser);
     } catch (error) {
       return HttpResponse.badRequest(error);
