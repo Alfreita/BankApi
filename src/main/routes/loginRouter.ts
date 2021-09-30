746beb748d584bd30bd3f76971ce0dfad8b8f5ec
Victor Almeida <victorf@ciandt.com>
2021-09-30 13:58:45 -0300

    refactor

--- src/main/routes/loginRouter.ts
+++ src/main/routes/loginRouter.ts
@@ -1,30 +1,20 @@
+import { Router } from "express";
 import { adaptRoute } from "../adapters/expressRouteAdapter";
 import SigninRouterComposer from "../composers/signinRouterComposer";
 import SignUpRouterComposer from "../composers/signUpRouterComposer";
-import { Router, Request, Response, NextFunction } from "express";
 import {
+  validate,
   validateSignin,
   validateSignUp,
-  validate,
 } from "../middlewares/expressValidator";
 
 const LoginRouter = (router: Router) => {
   router
     .route("/signin")
-    .post(
-      validateSignin,
-      validate,
-      (Req: Request, Res: Response, Next: NextFunction) =>
-        adaptRoute(SigninRouterComposer.compose())
-    );
+    .post(validateSignin, validate, adaptRoute(SigninRouterComposer.compose()));
   router
     .route("/signup")
-    .post(
-      validateSignUp,
-      validate,
-      (Req: Request, Res: Response, Next: NextFunction) =>
-        adaptRoute(SignUpRouterComposer.compose())
-    );
+    .post(validateSignUp, validate, adaptRoute(SignUpRouterComposer.compose()));
 };
 
 export default LoginRouter;
